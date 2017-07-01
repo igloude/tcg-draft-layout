7c10e732ed42680db505b4126d0fe4b9c7de7540
ian.gloude <ian.gloude@code42.com>
2017-07-01 14:55:00 -0500

    move to vue.js setup basic vues setup local json for testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
+var draft = new Vue({
+    el: '#draft',
+    data: {
+        description: {
+            long: 'Khans of Tarkir',
+            short: 'KTK'
+        },
+        gameProgress: {
+            pack: 1,
+            pick: 3
+        },
+        players: 3,
+        pack: pack // NOTE - this calls local data.json file referenced in the head
+    }
+})
 
+var player = new Vue({
+    el: '#player',
 
-$.getJSON('https://api.magicthegathering.io/v1/sets/ktk/booster', function(pack) {
-    console.log(pack.cards);
-});
+    data: {
+        picks: picks // NOTE - this calls local data.json file referenced in the head
+    }
+})
