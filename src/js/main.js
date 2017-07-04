514f0cef221040f4d9dfea1ffb1063a3a9aa4db0
ian.gloude <ian.gloude@code42.com>
2017-07-04 11:15:39 -0500

    added sorting functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 var draft = new Vue({
     el: '#draft',
+
     data: {
         description: {
             long: 'Khans of Tarkir',
@@ -18,6 +19,26 @@ var player = new Vue({
     el: '#player',
 
     data: {
+        sortKey: 'pick',
         picks: picks // NOTE - this calls local data.json file referenced in the head
+    },
+
+    methods: {
+        aFirst: function (picks) { // return picks sorted alpha a-z
+            this.sortKey = 'a';
+            this.picks = _.orderBy(picks, 'name');
+        },
+        zFirst: function (picks) { // return picks sorted alpha z-a
+            this.sortKey = 'z';
+            this.picks = _.orderBy(picks, 'name', 'desc');
+        },
+        cost: function (picks) { // return picks sorted by cost
+            this.sortKey = 'cost';
+            this.picks = _.orderBy(picks, ['cmc', 'name']);
+        },
+        pick: function(picks) {
+            this.sortKey = 'pick';
+            this.picks = _.orderBy(picks, 'pickOrder');
+        }
     }
 })
