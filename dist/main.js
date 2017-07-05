5aa551762f29f8067246af5264de9a7dc405bbd2
ian.gloude <ian.gloude@code42.com>
2017-07-05 10:21:56 -0500

    un-ignored dist for gh-pages branch

---
+++ dist/main.js
@@ -0,0 +1,60 @@
+var draft = new Vue({
+    el: '#draft',
+
+    data: {
+        description: {
+            long: 'Khans of Tarkir',
+            short: 'KTK'
+        },
+        gameProgress: {
+            pack: 1,
+            pick: 1
+        },
+        players: 3,
+        pack: pack // NOTE - this calls local data.json file referenced in the head
+    },
+
+    methods: {
+        pickCard: function(card) {
+            // NOTE - API call
+
+            // remove chosen card from pack
+            this.pack.splice(this.pack.indexOf(card), 1)
+
+            // set pickOrder on card and increment counter
+            card.pickOrder = this.gameProgress.pick;
+            this.gameProgress.pick++;
+
+            // add chosen card to picks
+            player.picks.push(card);
+        }
+    }
+})
+
+var player = new Vue({
+    el: '#player',
+
+    data: {
+        sortKey: 'pick',
+        picks: picks // NOTE - this calls local data.json file referenced in the head
+    },
+
+    methods: {
+        sortByA: function (picks) {
+            this.sortKey = 'a';
+            this.picks = _.orderBy(picks, ['name', 'cmc']);
+        },
+        sortByZ: function (picks) {
+            this.sortKey = 'z';
+            this.picks = _.orderBy(picks, ['name', 'cmc'], 'desc');
+        },
+        sortByCost: function (picks) {
+            this.sortKey = 'cost';
+            this.picks = _.orderBy(picks, ['cmc', 'name']);
+        },
+        sortByPick: function(picks) {
+            this.sortKey = 'pick';
+            this.picks = _.orderBy(picks, 'pickOrder');
+        }
+    }
+})
