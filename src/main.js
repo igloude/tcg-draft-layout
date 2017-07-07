454243842b400c51fe1f79f246ad775ac4bb5774
ian.gloude <ian.gloude@code42.com>
2017-07-07 17:01:00 -0500

    fixed ALL the bugs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Draft from './components/Draft.vue'
-import Player from './components/Player.vue'
+// import Player from './components/Player.vue'
 
 require('./scss/main.scss');
 
-var player = new Vue({
-        el: '#player',
-        render: h => h(Player)
-})
+// new Vue({
+//     el: '#player',
+//     render: h => h(Player),
+// })
 
-var draft = new Vue({
-        el: '#draft',
-        render: h => h(Draft)
+new Vue({
+    el: '#draft',
+    render: h => h(Draft)
 })
